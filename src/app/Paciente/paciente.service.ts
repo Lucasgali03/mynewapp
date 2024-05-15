a863577a7ba07098d3b3e1c8d69d2fe712d6c969
Lucas Galindo <l.galindoc2@uniandes.edu.co>
2024-05-15 10:45:42 -0500

    final

--- src/app/Paciente/paciente.service.ts
+++ src/app/Paciente/paciente.service.ts
@@ -6,7 +6,7 @@ import { PacienteDetail } from './paciente-detail';
 
 @Injectable()
 export class PacienteService {
-    private apiUrl: string = environment.baseUrl + 'pacientes';
+    private apiUrl: string = "https://gist.githubusercontent.com/k-garces/17c74c4ce5f6b09583fd9fcd941a7b68/raw/d9119d1d89f6fe4dea0250754baf11ea9dad1457/pacients.json";
     constructor(private http: HttpClient) { }
 
     getPacientes(): Observable<PacienteDetail[]> {
--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -5,17 +5,18 @@ import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 
 import { PacienteDetailComponent } from './Paciente/paciente-detail/paciente-detail.component';
+import { PacienteModule } from "./Paciente/Paciente.module";
 
 @NgModule({
-  declarations: [
-    AppComponent,
-
-  ],
-  imports: [
-    BrowserModule,
-    AppRoutingModule
-  ],
-  providers: [provideClientHydration()],
-  bootstrap: [AppComponent]
+    declarations: [
+        AppComponent,
+    ],
+    providers: [provideClientHydration()],
+    bootstrap: [AppComponent],
+    imports: [
+        BrowserModule,
+        AppRoutingModule,
+        PacienteModule
+    ]
 })
 export class AppModule { }
